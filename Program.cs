cb50cfe503b9c8d7c3031009c883072653f99c94
lavanyaboddu31@gmail.com <lavanyaboddu31@gmail.com>
2023-07-13 10:17:49 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp__Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] marks = new int[5];
+            Console.WriteLine("Enter Marks");
+            for (int i = 0; i < marks.Length; i++)
+            {
+                Console.WriteLine("Enter marks in Subject " + (i + 1));
+                marks[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Marks List");
+            for (int i = 0; i < marks.Length; i++)
+            {
+                Console.WriteLine($"Subject {i + 1} \t Marks: {marks[i]}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
